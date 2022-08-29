d38438569b7c892523b3fd434b04d5857ad65101
sangwoo <sangwoo@sangwooui-MacBookPro.local>
2022-08-29 17:58:30 +0900

    [ADD] initializing

---
+++ server.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { createApp } = require("./app");
+const { appDataSource } = require("./src/models/dataSource");
+const { logger } = require("./src/logs/config/winston");
+
+const startServer = async ()=>{
+    try{
+        const PORT = process.env.PORT
+        const app = createApp();
+
+        await appDataSource.initialize();
+
+        app.get("/ping", (req,res,next)=>{
+            logger.info('GET /ping');
+            res.status(200).json({message : "pong"});
+        })
+
+        app.listen(PORT, ()=>{
+           logger.info(`listening on PORT : ${PORT}`)
+        });
+        } catch(err){
+        logger.error(err)
+        }
+}
+startServer();
