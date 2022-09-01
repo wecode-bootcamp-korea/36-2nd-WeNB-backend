90ca3be5657478e231a083be5d948f182958e871
sangwoo <sangwoo@sangwooui-MacBookPro.local>
2022-09-01 11:16:43 +0900

    [ADD] Image File AWS-S3 Upload system

---
+++ src/middlewares/imageUploader.js
@@ -0,0 +1,34 @@
+const aws = require('aws-sdk')
+const multer = require('multer')
+const multers3 = require('multer-s3')
+const path = require('path')
+
+aws.config.update({
+    region: process.env.region,
+    accessKeyId: process.env.accessKeyId,
+    secretAccessKey: process.env.secretAccessKey,
+});
+
+const s3 = new aws.S3();
+
+const fileFormat = ['.png', '.jpg', '.jpeg', '.bmp'];
+
+const imageUploader = multer({
+    storage: multers3({
+        s3: s3,
+        bucket: 'wenb-s3',
+        acl: 'public-read-write',
+        key: (req, file, cb) => {
+            const directory = req.query.directory ?? ''
+            const extension = path.extname(file.originalname)
+            if(!fileFormat.includes(extension)){
+                return cb(new Error('WRONG_EXTENSION'))
+            }
+            cb(null, `${directory}/${file.originalname}`
+        )}
+    }),
+})
+
+module.exports = {
+    imageUploader
+}
